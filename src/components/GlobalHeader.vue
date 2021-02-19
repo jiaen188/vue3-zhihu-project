9d191d14925de7e543c617b4ef6bd33231aefbbc
gulifan <gulifan@dxy.cn>
2021-02-19 21:45:37 +0800

    feat: dropdown组件基本功能

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,18 +1,21 @@
 <template>
   <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
-    <a class="navbar-brand" href="#">者也专栏</a>
+    <a class="navbar-brand" href="#">嘉恩专栏</a>
     <ul v-if="!user.isLogin" class="list-inline mb-0">
       <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">登陆</a></li>
       <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
     </ul>
     <ul v-else class="list-inline mb-0">
-      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">你好 {{user.name}}</a></li>
+      <li class="list-inline-item">
+        <Dropdown title="你好 嘉恩"></Dropdown>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import Dropdown from './Dropdown.vue'
 
 export interface UserProps {
   isLogin: boolean;
@@ -22,6 +25,9 @@ export interface UserProps {
 
 export default defineComponent({
   name: 'GlobalHeader',
+  components: {
+    Dropdown
+  },
   props: {
     user: {
       type: Object as PropType<UserProps>,
