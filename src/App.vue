a0c1abb34d804d14b22ad8226e8501708817f7bb
gulifan <gulifan@dxy.cn>
2021-02-19 21:06:53 +0800

    feat: globalHeader 组件开发

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list" />
   </div>
 </template>
@@ -8,6 +9,12 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'Gavin'
+}
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -38,11 +45,13 @@ const testData: ColumnProps[] = [
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup () {
     return {
-      list: testData
+      list: testData,
+      currentUser
     }
   }
 })
