4691abda3866b3b4e340cf2f434c4d8307b58f33
gulifan <gulifan@dxy.cn>
2021-03-15 21:54:40 +0800

    feat: 使用async 和await 改造

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,4 @@
-import { createStore } from 'vuex'
+import { Commit, createStore } from 'vuex'
 import axios from 'axios'
 
 interface UserProps {
@@ -37,6 +37,11 @@ export interface GlobalDataProps {
   user: UserProps;
 }
 
+const getAndCommit = async (url: string, mutationName: string, commit: Commit) => {
+  const { data } = await axios.get(url)
+  commit(mutationName, data)
+}
+
 const store = createStore<GlobalDataProps>({
   state: {
     colums: [],
@@ -64,19 +69,13 @@ const store = createStore<GlobalDataProps>({
   },
   actions: {
     fetchColumns ({ commit }) {
-      axios.get('/columns').then((res: { data: ColumnProps[] }) => {
-        commit('fetchColumns', res.data)
-      })
+      getAndCommit('/columns', 'fetchColumns', commit)
     },
-    fetchColumn ({ commit }, cid) {
-      axios.get(`/columns/${cid}`).then(res => {
-        commit('fetchColumn', res.data)
-      })
+    async fetchColumn ({ commit }, cid) {
+      getAndCommit(`/columns/${cid}`, 'fetchColumn', commit)
     },
-    fetchPosts ({ commit }, cid) {
-      axios.get(`/columns/${cid}/posts`).then(res => {
-        commit('fetchPosts', res.data)
-      })
+    async fetchPosts ({ commit }, cid) {
+      getAndCommit(`/columns/${cid}/posts`, 'fetchPosts', commit)
     }
   },
   getters: {
