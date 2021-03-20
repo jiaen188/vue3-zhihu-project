333cb413100d7ffcbafdb2292e7477b45ba7a8c6
gulifan <gulifan@dxy.cn>
2021-03-20 22:38:14 +0800

    feat: loader组件编写

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <h1 v-if="isLoading">正在读取...</h1>
+    <Loader text="拼命加载中" background="rgba(0, 0, 0, 0.8)" />
     <router-view></router-view>
     <footer class="text-center py-4 text-secondary bg-light mt-6">
       <small>
@@ -19,15 +19,17 @@
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue'
+import { useStore } from 'vuex'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalHeader from './components/GlobalHeader.vue'
-import { useStore } from 'vuex'
 import { GlobalDataProps } from './store'
+import Loader from './components/Loader.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    GlobalHeader
+    GlobalHeader,
+    Loader
   },
   setup () {
     const store = useStore<GlobalDataProps>()
